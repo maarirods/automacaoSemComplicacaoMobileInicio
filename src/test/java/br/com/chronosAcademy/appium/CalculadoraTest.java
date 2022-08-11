479881d15266612beb53e9d166a3c60700eeda6e
maarirods <rodspersonal22@gmail.com>
2022-08-11 12:20:21 -0300

    aula before after metodos final

--- src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
+++ src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
@@ -1,29 +1,86 @@
 package br.com.chronosAcademy.appium;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
-import java.sql.Driver;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
+    DriverFactory driverFactory;
 
-    @Test
-    public void validarMultiplicacao() throws MalformedURLException {
-        DriverFactory driverFactory = new DriverFactory();
+    @BeforeEach
+    public void inicializarDriver() throws MalformedURLException {
+        driverFactory = new DriverFactory();
         driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2",
                 "com.android.calculator2", "com.android.calculator2.Calculator");
         driverFactory.setDriver();
-        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
-        driverFactory.getDriver().findElement(MobileBy.id("op_add")).click();
-        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
-        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+    }
+
+    @AfterEach
+    public void finalizaDriver(){
+        driverFactory.getDriver().quit();
+    }
+
+
+    @Test
+    public void validarMutiplicacao(){
+        clickId("digit_7");
+        clickId("op_mul");
+        clickId("digit_9");
+        clickId("eq");
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+
+        assertEquals("63", resultado);
+
+
+    }
+
+    @Test
+    public void validarSoma(){
+        clickId("digit_7");
+        clickId("op_add");
+        clickId("digit_9");
+        clickId("eq");
+
         String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
 
         assertEquals("16", resultado);
-        driverFactory.getDriver().quit();
 
     }
+
+    @Test
+    public void validarSubtracao(){
+        clickId("digit_7");
+        clickId("op_sub");
+        clickId("digit_9");
+        clickId("eq");
+
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+
+        assertEquals("−2", resultado);
+
+    }
+
+    @Test
+    public void validarDivisao(){
+        clickId("digit_7");
+        clickId("op_div");
+        clickId("digit_9");
+        clickId("eq");
+
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+
+        assertEquals("0.777777777777", resultado);
+
+    }
+
+    public void clickId(String id){
+        driverFactory.getDriver().findElement(MobileBy.id(id)).click();
+    }
+
+
 }
